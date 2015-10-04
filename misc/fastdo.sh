9e23ba463069db7dbea68b57a71d144a0baa5798
butterworth <butterworth@163.com>
2015-10-04 18:44:27 +0800

    modify fastdo.sh

--- misc/fastdo.sh
+++ misc/fastdo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SHARE_WIN_LIN_FILE=/media/EXT4_300/CODE/smdk6410/temp_share_for_other_proj/smdk6410_toFixfsMount/u-boot
+#copy_imgs_name=u-boot.bin
 
 function current_script_path()
 {
@@ -8,18 +8,94 @@ function current_script_path()
   echo -e "\nCurrent script path: $filepath"
 }
 
+# Note:
+# If you want to use checkpfrest value, you should call checkout_path_file_exist at first()"
+function checkout_path_file_exist()
+{
+  echo -e "\$@ = \"$@\""
+  specific_contents=$3
+  checkpfrest=1
+
+  case $1 in
+  d)
+     if [ ! -d $2 ]; then
+       echo "The $specific_contents path: \"$2\" is not exist."
+       checkpfrest=1
+     else
+       echo -e "The $specific_contents path is \"$2\"\n"
+       checkpfrest=0
+     fi
+     ;;
+  f)
+     if [ ! -f $2 ]; then
+       echo "The $specific_contents file: \"$2\" is not exist."
+       checkpfrest=1
+     else
+       echo -e "The $specific_contents file is \"$2\"\n"
+       checkpfrest=0
+     fi
+     ;;
+  *)
+     echo "the 1st parameter is error."
+     checkpfrest=1
+     ;;
+  esac
+}
+
+function create_share_win_lin_path()
+{
+  parameter_file=$SCRIPTDIR/.paramfile
+  if [ ! -f $parameter_file ]; then
+      echo ""
+      read -p "Please supply the root SHARE path for sharing image:  "  SHARE_ROOT_PATH
+#     rest=`checkout_path_file_exist d $SHARE_ROOT_PATH "root share file"`
+#     rest=$(checkout_path_file_exist  d  $SHARE_ROOT_PATH  "root share file")
+#     if [ $rest -ne 0 ]; then
+#     if [ "$rest" = "0" ]; then
+      checkout_path_file_exist  d  $SHARE_ROOT_PATH  "root share file"  # 和$()还是有区别的，$()赋值后还需要echo调用才去调用函数
+      if [ $checkpfrest -ne 0 ]; then
+        exit
+      else
+        echo "SHARE_ROOT_PATH=$SHARE_ROOT_PATH"  >  $parameter_file
+      fi
+
+      read -p "Please supply the share file path based $SHARE_ROOT_PATH :  "  SHARE_IMGS_FILE
+      SHARE_IMGS_FULL_PATH=$SHARE_ROOT_PATH/$SHARE_IMGS_FILE
+      checkout_path_file_exist  d  $SHARE_IMGS_FULL_PATH  "share file"
+      if [ $checkpfrest -ne 0 ]; then
+        mkdir -p $SHARE_IMGS_FULL_PATH
+        echo "The share file path \"$SHARE_IMGS_FULL_PATH\" now is created."
+      else
+        rm $SHARE_IMGS_FULL_PATH/u-boot.bin
+      fi
+      echo "SHARE_IMGS_FILE=$SHARE_IMGS_FILE"  >> $parameter_file
+  else
+      SHARE_ROOT_PATH=`grep "SHARE_ROOT_PATH="  $parameter_file | awk -F '=' '{print $2}'`
+      checkout_path_file_exist  d  $SHARE_ROOT_PATH "root share file"
+      if [ $checkpfrest -ne 0 ]; then
+        exit
+      fi
+
+      SHARE_IMGS_FILE=`grep "SHARE_IMGS_FILE="  $parameter_file  | awk -F '=' '{print $2}'`
+      SHARE_IMGS_FULL_PATH=$SHARE_ROOT_PATH/$SHARE_IMGS_FILE
+      checkout_path_file_exist  d  $SHARE_IMGS_FULL_PATH  "image file"
+      if [ $checkpfrest -ne 0 ]; then
+        mkdir -p $SHARE_IMGS_FULL_PATH
+        echo "The share file path \"$SHARE_IMGS_FULL_PATH\" now is created."
+      else
+        rm $SHARE_IMGS_FULL_PATH/u-boot.bin
+      fi
+  fi
+}
+
 function copy_u-boot_img_to_share_file()
 {
   echo "list u-boot images:"
-  ls -laF u-boot*  
+  ls -laF u-boot*
 
-  if [ ! -f $SHARE_WIN_LIN_FILE ]; then
-    mkdir -p $SHARE_WIN_LIN_FILE
-  fi
-
-  echo "Copy u-boot.bin to $SHARE_WIN_LIN_FILE"
-  cp  u-boot.bin  $SHARE_WIN_LIN_FILE
-  ls -laF $SHARE_WIN_LIN_FILE
+  echo "Copy u-boot.bin to $SHARE_IMGS_FULL_PATH"
+  cp  u-boot.bin  $SHARE_IMGS_FULL_PATH
+  ls -laF $SHARE_IMGS_FULL_PATH
 
   echo -e "\nNow, you can transfer u-boot.bin from Virtual-Win8 to Note-PC by QQ."
 }
@@ -29,6 +105,7 @@ function u-boot_operate()
   cd ../uboot1.1.6
   current_script_path
 
+  create_share_win_lin_path
   copy_u-boot_img_to_share_file
 }
 
@@ -43,11 +120,11 @@ function kernel_operate()
 function part_operate_branch()
 {
   case "$BUILD_PART" in
-   'u'|'U')
+   "u"|"U")
         echo "you have selected [ u-boot ]"
         u-boot_operate
         ;;
-   'k'|'K')
+   "k"|"K")
         echo "you h4ave selected [ kernel ]"
         kernel_operate
         ;;
@@ -64,6 +141,10 @@ function list_build_part()
   echo "        * u <-> u-boot"
   echo "        * k <-> kernel"
 }
+
+SCRIPT_NAME=$0
+SCRIPTDIR=$(cd "$(dirname "$0")"; pwd)
+echo -e "SCRIPT: \n\t$SCRIPTDIR/$SCRIPT_NAME"
 
 echo -e "\nWhich part you want to operate ?"
 list_build_part
